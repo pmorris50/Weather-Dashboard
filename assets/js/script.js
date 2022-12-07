351314bbe644e42b1c09becc24d8176c024fb24a
pmorris50 <patrickmorris532@gmail.com>
2022-12-06 19:27:20 -0700

    weather data is console.logged, need to grab select data and render to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 //
 var citySearch = document.querySelector("#city");
 var weatherForm = document.querySelector('#weather-form');
-
+var diisplayCurrentWeather = document.querySelector('#current-weather');
 var apiKey =  '9cd3a752dd82b1bbbc1f1d19a793dcd8';
+
 
 
 
@@ -12,16 +13,40 @@ weatherForm.addEventListener('submit', function(e) {
     e.preventDefault();
     console.log(citySearch.value);
     fiveDay(citySearch.value);
+    currentWeather(citySearch.value)
 });
 
 function fiveDay(cityName){
-
+var units = 'imperial';
+var lang = 'en';
 console.log(cityName)
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`).then(function(responseBody){
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=${units}&lang=${lang}`).then(function(responseBody){
     console.log(responseBody);
     return responseBody.json()
 }).then(function(fiveDayForecast){
     console.log(fiveDayForecast);
 
 })
+
 }
+
+
+function currentWeather(cityName){
+var units = 'imperial';
+var lang = 'en';   
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}&lan=${lang})`).then(function(responseBody){
+console.log(responseBody);
+return responseBody.json()
+}).then(function(weather){
+    console.log(weather);
+  
+
+})
+}
+
+
+// function displayCurrentWeather(){
+
+
+
+// }
